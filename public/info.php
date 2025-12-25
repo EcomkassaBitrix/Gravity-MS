7a87c25a70e40541c6dc340f30d233e5c1f135fd
Смагин Сергей Валентинович <sergey@gravity-group.ru>
2025-12-25 18:48:42 +0500

    Отладка

--- public/info.php
+++ public/info.php
@@ -1,7 +1,11 @@
 <?php
 // Сравните на разных системах:
-echo "PHP Version: " . PHP_VERSION . "<br/>";
-echo "Architecture: " . (PHP_INT_SIZE * 8) . "-bit<br/>";
-echo "Float precision: " . PHP_FLOAT_DIG . "<br/>";
-echo "Value: " . 51933.20 . "<br/>";
-echo "JSON: " . json_encode(['sum' => round(51933.20, 2)]) . "<br/>";
+echo "PHP Version: " . PHP_VERSION . "<br/>\n";
+echo "Architecture: " . (PHP_INT_SIZE * 8) . "-bit<br/>\n";
+echo "Float precision: " . PHP_FLOAT_DIG . "<br/>\n";
+echo "Value: " . 51933.20 . "<br/>\n";
+echo "JSON (PHP_ROUND_HALF_UP): " . json_encode(['sum' => round(51933.20, 2, PHP_ROUND_HALF_UP)]) . "<br/>\n";
+echo "JSON (PHP_ROUND_HALF_DOWN): " . json_encode(['sum' => round(51933.20, 2, PHP_ROUND_HALF_DOWN)]) . "<br/>\n";
+echo "JSON (PHP_ROUND_HALF_ODD): " . json_encode(['sum' => round(51933.20, 2, PHP_ROUND_HALF_ODD)]) . "<br/>\n";
+
+phpinfo();
